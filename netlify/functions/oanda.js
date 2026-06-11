90e5918de3a0555fb8048e6ff33f825e68adefa4
punjabone12345 <punjabone12345@gmail.com>
2026-06-11 09:50:27 +0530

    Change OANDA_TOKEN to a new token value
    Updated OANDA_TOKEN with a new value.

--- netlify/functions/oanda.js
+++ netlify/functions/oanda.js
@@ -1,4 +1,4 @@
-const OANDA_TOKEN = "your_token";
+const OANDA_TOKEN = "3bd390b7df5a74cc7b41355f3680de4d-3d82122d27f629fe73efcc27f99d542a";
 const OANDA_ACCT = "101-001-38845298-002";
 const OANDA_BASE = "https://api-fxpractice.oanda.com/v3";
 
